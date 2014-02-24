256c2141dbe8b6d876578a08af829374fb188bc4
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-02-24 16:56:51 -0600

    minor edits

--- bimodal.test.r
+++ bimodal.test.r
@@ -1,7 +1,5 @@
-bimodal.test <- function(file_in="test_data.txt", primary_proportion=0.95, var_filter=1.5, method="npEM",file_out="bimodal.test.output.txt"){
+bimodal.test <- function(file_in="test_data.txt", primary_mix_proportion=0.95, var_filter=1.5, method="npEM",file_out="bimodal.test.output.txt"){
 
-
-  
 # supported methods, npEM, spEM, #normalmixEM#
 # require necessary package
 require(mixtools)||install.packages("mixtools")
@@ -14,14 +12,14 @@ require(mixtools)||install.packages("mixtools")
 # The non parametric EM seems to tolerate sparse count data reasoanbly well
 
 # with mu0 = 2
-# decides if data are unimodal or bimodal + based on proportion
+# decides if data are unimodal or bimodal + based on primary mix proportion
 
 cat(
     paste(
         "# file_in: ", "\t", file_in, "\n",   
         "# method: ",  "\t", method,  "\n",
         "# var_filter", "\t", var_filter, "\n",
-        "# primary_proportion: ", "\t", primary_proportion,"\n",
+        "# primary_mix_proportion: ", "\t", primary_mix_proportion,"\n",
          sep="",collapse=""
         ),
     file=file_out
@@ -34,7 +32,7 @@ num_cols <- ncol(my_data)
 
 output <- matrix("", num_rows, 5)
 dimnames(output)[[1]] <- dimnames(my_data)[[1]]
-dimnames(output)[[2]] <- c("var", "max_portion", "min_portion", "modality_guess", "guess_on")
+dimnames(output)[[2]] <- c("var", "max_proportion", "min_proportion", "modality_guess", "guess_on")
 
 for (i in 1:num_rows){
 
@@ -51,12 +49,12 @@ for (i in 1:num_rows){
             my_npEM <- npEM(my_data[i,], mu0=2, verb=FALSE)
             my_lambdahat <- sort(my_npEM$lambdahat,decreasing=TRUE)
             output[i,2:3] <- my_lambdahat
-            if  ( my_lambdahat[1] >= primary_proportion ){
+            if  ( my_lambdahat[1] >= primary_mix_proportion ){
                 output[i,4] <- "1"
-                output[i,5] <- "primary_proportion"
+                output[i,5] <- "primary_mix_proportion"
             } else {
                 output[i,4] <- "2 or more"
-                output[i,5] <- "primary_proportion"
+                output[i,5] <- "primary_mix_proportion"
             }
         }
 
@@ -64,12 +62,12 @@ for (i in 1:num_rows){
             my_spEM <- spEM(my_data[i,], mu0=2, verb=FALSE)
             my_lambdahat <- sort(my_spEM$lambdahat,decreasing=TRUE)
             output[i,2:3] <- my_lambdahat
-            if  ( my_lambdahat[1] >= primary_proportion ){
+            if  ( my_lambdahat[1] >= primary_mix_proportion ){
                 output[i,4] <- "1"
-                output[i,5] <- "primary_proportion"
+                output[i,5] <- "primary_mix_proportion"
             } else {
                 output[i,4] <- "2 or more"
-                output[i,5] <- "primary_proportion"
+                output[i,5] <- "primary_mix_proportion"
             }
         }
 
@@ -94,7 +92,7 @@ for (i in 1:num_rows){
 
 }
 
-write.table(output, file=file_out, col.names=NA, row.names=TRUE, append=TRUE, sep="\t")
+suppressWarnings(write.table(output, file=file_out, col.names=NA, row.names=TRUE, append=TRUE, sep="\t"))
 
 
 }
